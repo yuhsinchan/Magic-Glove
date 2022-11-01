e8dbf6e0aa43d62ed908ae8f5d1c1f4ca8b36742
JingYuan <b08901081@ntu.edu.tw>
2022-11-01 16:18:47 +0800

    update AudDSP.sv and pass testbench

--- lab3/src/AudDSP.sv
+++ lab3/src/AudDSP.sv
@@ -17,7 +17,8 @@ module AudDSP (
 
     localparam IDLE  = 0; // stop  will reset the state to IDLE
     localparam PLAY  = 1; // start will   set the state to PLAY
-    localparam PAUSE = 2; // pause will   set the state to PAUSE 
+    localparam PAUSE = 2; // pause will   set the state to PAUSE
+    localparam WAIT  = 3;
     
     localparam SP_NORMAL = 0;
     localparam SP_FAST   = 1;
@@ -32,28 +33,32 @@ module AudDSP (
     logic signed [15:0] dac_data_w, dac_data_r;
     logic signed [15:0] pre_dac_data_w, pre_dac_data_r;
     logic        [19:0] sram_addr_w, sram_addr_r;
-    
 
-    assign o_dac_data = dac_data_r;
+    logic daclrck_w, daclrck_r;
+    logic posedge_daclrck;    
+
+    assign o_dac_data  = dac_data_r;
     assign o_sram_addr = sram_addr_r;
-    assign o_sram_addr = sram_addr_r;
+    assign posedge_daclrck = i_daclrck & ~daclrck_r;
 
 
     always_comb begin
-        state_w = state_r;
-        speed_state_w = speed_state_r;
-        sampling_counter_w =  sampling_counter_r;
-        dac_data_w = dac_data_r;
-        pre_dac_data_w = pre_dac_data_r;
-        sram_addr_w = sram_addr_r;
+        state_w            = state_r;
+        speed_state_w      = speed_state_r;
+        sampling_counter_w = sampling_counter_r;
+        dac_data_w         = dac_data_r;
+        pre_dac_data_w     = pre_dac_data_r;
+        sram_addr_w        = sram_addr_r;
+        daclrck_w          = daclrck_r;
         
+        daclrck_w = i_daclrck;
         if (i_fast) begin
             speed_state_w = SP_FAST;
         end
-        else if (i_slow0) begin
+        else if (i_slow_0) begin
             speed_state_w = SP_SLOW0;
         end
-        else if (i_slow1) begin
+        else if (i_slow_1) begin
             speed_state_w = SP_SLOW1;
         end
         else begin
@@ -63,12 +68,11 @@ module AudDSP (
         case (state_r)         
             IDLE: begin
                 if (i_start) begin
-                    state_w = PLAY;
+                    state_w = WAIT;
                 end
             end
             PLAY: begin
-                pre_dac_data_w = dac_data_r;
-                
+                // $display("PLAY");
                 if (i_stop) begin
                     state_w = IDLE;
                 end
@@ -78,36 +82,44 @@ module AudDSP (
                 else begin
                     case (speed_state_r)
                         SP_NORMAL: begin
+                            // $display("Normal");
                             dac_data_w = $signed(i_sram_data);
-                            sram_addr_w = sram_addr_r + 19'd1; 
+                            sram_addr_w = sram_addr_r + 20'b1; 
                         end
                         SP_FAST: begin
+                            // $display("Fast");
                             // down sampling
                             dac_data_w = $signed(i_sram_data);
-                            sram_addr_w = sram_addr_r + {17'b0, (i_speed + 3'd1)}; 
+                            sram_addr_w = sram_addr_r + {17'b0, (i_speed + 3'b1)}; 
                         end
                         SP_SLOW0: begin
+                            // $display("Slow0");
                             // up sampling: piecewise-constant interpolation
-                            sampling_counter_w = sampling_counter_r + 3'd1;
                             dac_data_w = $signed(pre_dac_data_r);
-                            if (sampling_counter_r = i_speed) begin
-                                sampling_counter_w = 3'd0;
-                                sram_addr_w = sram_addr_r + 19'd1;
+                            sampling_counter_w = sampling_counter_r + 3'b1;
+                            if (sampling_counter_r == i_speed) begin
+                                sampling_counter_w = 3'b0;
+                                sram_addr_w = sram_addr_r + 20'b1;
+                                pre_dac_data_w = $signed(i_sram_data);
                             end
                         end
                         SP_SLOW1: begin
+                            // $display("Slow1");
                             // up sampling: linear interpolation
-                            sampling_counter_w = sampling_counter_r + 3'd1;
-                            dac_data_w = $signed( $signed(dac_data_r - pre_dac_data_r) / $signed(i_speed - sampling_counter_r)) + $signed(pre_dac_data_r);
-                            if (sampling_counter_r = i_speed) begin
-                                
+                            dac_data_w = $signed( $signed(i_sram_data - pre_dac_data_r) * $signed({13'b0, sampling_counter_r}) / $signed({12'b0, i_speed} + 15'b1) ) + $signed(pre_dac_data_r);
+                            sampling_counter_w = sampling_counter_r + 3'b1;
+                            if (sampling_counter_r == i_speed) begin
+                                sampling_counter_w = 3'b0;
+                                sram_addr_w = sram_addr_r + 20'b1;
+                                pre_dac_data_w = $signed(i_sram_data);
                             end
                         end
                     endcase
                 end
+                state_w = WAIT;
             end
             PAUSE: begin    
-                o_dac_data_w = 15'b0;
+                dac_data_w = 16'b0;
                 if (i_stop) begin
                     state_w = IDLE;
                 end
@@ -115,8 +127,13 @@ module AudDSP (
                     state_w = PLAY;
                 end
             end
+            WAIT: begin
+                if (posedge_daclrck) begin
+                    state_w = PLAY;
+                end
+            end
             default: begin
-                out_dac_data = 15'b0;
+                dac_data_w = 16'b0;
             end
         endcase
     end
@@ -124,19 +141,22 @@ module AudDSP (
     always_ff @(posedge i_clk or negedge i_rst_n) begin
         if (!i_rst_n) begin
             // reset
-            state_r <= 0;
-            speed_state_r <= 0;
-            sampling_counter_r <= 0;
-            dac_data_r <= 0;
-            pre_dac_data_r <= 0;
-            sram_addr_r <= 1;
+            // $display("RESTART");
+            state_r            <= IDLE;
+            speed_state_r      <= SP_NORMAL;
+            sampling_counter_r <= 3'b0;
+            dac_data_r         <= 16'b0;
+            pre_dac_data_r     <= 16'b0;
+            sram_addr_r        <= 20'b0;
+            daclrck_r 	       <= i_daclrck;
         end else begin
-            state_r <= state_w;
-            speed_state_r <= speed_state_w;
-            sampling_counter_r <=  sampling_counter_w;
-            dac_data_r <= dac_data_w;
-            pre_dac_data_r <= pre_dac_data_w;
-            sram_addr_r <= sram_addr_w;
+            state_r            <= state_w;
+            speed_state_r      <= speed_state_w;
+            sampling_counter_r <= sampling_counter_w;
+            dac_data_r         <= dac_data_w;
+            pre_dac_data_r     <= pre_dac_data_w;
+            sram_addr_r        <= sram_addr_w;
+            daclrck_r   	   <= daclrck_w;
         end
     end
 
--- lab3/src/tb_verilog/test_AudDSP.sv
+++ lab3/src/tb_verilog/test_AudDSP.sv
@@ -1,47 +1,52 @@
-`timescale 1ns/10ps
+// `timescale 1ns/10ps
 
 module tb;
-  localparam End_Address = 20'd8; // wm8731 address
   localparam CLK = 10;
 	localparam HCLK = CLK/2;
 
-  initial clk = 0;
-	always #HCLK clk = ~clk;
+  localparam DACLRCK = 160;
+	localparam BIG_HCLK = DACLRCK/2;
 
-  logic rst;
-  logic clk;
-  logic start;
-  logic pause;
-  logic stop;
-  logic speed;
-  logic fast;
-  logic slow_0;
-  logic slow_1;
-  logic daclrck;
+
+  logic        rst;
+  logic        clk;
+  logic        start;
+  logic        pause;
+  logic        stop;
+  logic [2:0]  speed;
+  logic        fast;
+  logic        slow_0;
+  logic        slow_1;
+  logic        daclrck;
   logic [15:0] cur_data;
-  logic [15:0] sram_data [0:7]; // 8 data each 16 bits
+  logic [15:0] sram_data [0:15]; // 8 data each 16 bits
   logic [15:0] out_data;
   logic [19:0] next_addr;
   
+  initial clk = 0;
+	always #HCLK clk = ~clk;
+  initial daclrck = 0;
+  always #BIG_HCLK daclrck = ~daclrck;
+
   AudDSP dsp0(
-    .i_rst_n        (rst),
-    .i_clk          (clk),
-    .i_start        (start),
-    .i_pause        (pause),
-    .i_stop         (stop),
-    .i_speed        (speed),
-    .i_fast         (fast),
-    .i_slow_0       (slow_0), // constant interpolation
-    .i_slow_1       (slow_1), // linear interpolation
-    .i_daclrck      (daclrck),
-    .i_sram_data    (cur_data),
-    .o_dac_data     (out_data),
-    .o_sram_addr    (next_addr)
+    .i_rst_n(rst),
+    .i_clk(clk),
+    .i_start(start),
+    .i_pause(pause),
+    .i_stop(stop),
+    .i_speed(speed),
+    .i_fast(fast),
+    .i_slow_0(slow_0), // constant interpolation
+    .i_slow_1(slow_1), // linear interpolation
+    .i_daclrck(daclrck),
+    .i_sram_data(cur_data),
+    .o_dac_data(out_data),
+    .o_sram_addr(next_addr)
   );
 
   initial begin
-    for (int i = 0; i < 8; i = i + 1) begin
-      sram_data[i] = i * 16;
+    for (int i = 0; i < 16; i = i + 1) begin
+      sram_data[i] = i * 32;
     end
   end
 
@@ -54,40 +59,40 @@ module tb;
     start  <= 0;
     pause  <= 0;
     stop   <= 0;
-    speed  <= 7;
+    speed  <= 3;
     fast   <= 0;
     slow_0 <= 0;
     slow_1 <= 1;
     rst    <= 1;
+    cur_data = sram_data[0];
 
 
-    rst <= 1;
 		#(2*CLK)
 		rst <= 0;
     @(posedge clk) rst <= 1;
-
 
     start <= 1;
     #(CLK)
     start <= 0;
     
     $display("=========");
-    $display("data[%1d] = %2d ", 0, cur_data);
-    $display("out_data = %2d", out_data);
-    $display("next_addr = %2d", next_addr - 1);
-    $display("=========");
+    $display("data[%2d]  = %2d", 0, cur_data);
+    @(posedge daclrck) begin
+      $display("out_data  = %2d", out_data);
+      $display("next_addr = %2d", next_addr);
+    end
+    
     do begin
       cur_data = sram_data[next_addr];
       $display("=========");
-			$display("data[%1d] = %2d ", next_addr - 1, data);
-			@(negedge daclrck)
-			$display("out_data = %2d", out_data);
-      $display("next_addr = %2d", next_addr - 1);
-			$display("=========");
-		end while (next_addr <= end_addr);
+      $display("data[%2d]  = %2d", next_addr, cur_data);
+      @(posedge daclrck) begin
+        $display("out_data  = %2d", out_data);
+        $display("next_addr = %2d", next_addr);
+      end
+    end while (next_addr <= 3);
     $finish;
-	
-  end
+	end
 
   initial begin
 		#(500000*CLK)
