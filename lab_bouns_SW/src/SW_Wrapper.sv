a6e82fd0d32327808d6814780224189260281452
allenwu9999 <b08901057@ntu.edu.tw>
2022-11-12 04:16:36 +0800

    wrapper transmission done, but calculation not yet verified

--- lab_bouns_SW/src/SW_Wrapper.sv
+++ lab_bouns_SW/src/SW_Wrapper.sv
@@ -38,6 +38,15 @@ localparam S_CALC = 2;
 localparam S_QUERY_TX = 3;
 localparam S_WRITE = 4;
 
+// local paramenters
+logic [2:0] state_r, state_w;
+logic [6:0] bytes_counter_r, bytes_counter_w;
+logic [247:0] result_r, result_w;
+logic [4:0] avm_address_w, avm_address_r;
+logic avm_read_r, avm_read_w;
+logic avm_write_r, avm_write_w;
+
+
 // module output assignment
 assign avm_writedata = result_r[247-:8];
 assign avm_address = avm_address_r;
@@ -47,36 +56,31 @@ assign avm_write = avm_write_r;
 // parameters communicating with core
 logic [2*`REF_MAX_LENGTH-1:0] ref_r, ref_w;
 logic [2*`READ_MAX_LENGTH-1:0] read_r, read_w;
-logic [`DP_SW_SCORE_BITWIDTH-1:0] alignment_score_r;
-logic [$clog2(`REF_MAX_LENGTH)-1:0] column_r;
-logic [$clog2(`READ_MAX_LENGTH)-1:0] row_r;
-logic core_read_ready_r, core_write_ready_r;
+logic [$clog2(`REF_MAX_LENGTH):0] ref_length_r;
+logic [$clog2(`READ_MAX_LENGTH):0] read_length_r;
+logic signed [`DP_SW_SCORE_BITWIDTH-1:0] alignment_score;
+logic [$clog2(`REF_MAX_LENGTH)-1:0] column;
+logic [$clog2(`READ_MAX_LENGTH)-1:0] row;
+logic core_read_ready_w, core_write_ready_w;
 logic wrapper_read_ready_w, wrapper_write_ready_w, wrapper_read_ready_r, wrapper_write_ready_r;
 
-// local paramenters
-logic [2:0] state_r, state_w;
-logic [6:0] bytes_counter_r, bytes_counter_w;
-logic [247:0] result_r, result_w;
-logic [4:0] avm_address_w, avm_address_r;
-logic avm_read_r, avm_read_w;
-logic avm_write_r, avm_write_w;
 // Remember to complete the port connection
 SW_core sw_core(
     .clk				(avm_clk),
     .rst				(avm_rst),
 
-	.o_ready			(core_read_ready_r),
+	.o_ready			(core_read_ready_w),
     .i_valid			(wrapper_read_ready_r),
     .i_sequence_ref		(ref_r),
     .i_sequence_read	(read_r),
-    .i_seq_ref_length	(`REF_LENGTH),
-    .i_seq_read_length	(`READ_LENGTH),
+    .i_seq_ref_length	(ref_length_r),
+    .i_seq_read_length	(read_length_r),
     
     .i_ready			(wrapper_write_ready_r),
-    .o_valid			(core_write_ready_r),
-    .o_alignment_score	(alignment_score_r),
-    .o_column			(column_r),
-    .o_row				(row_r)
+    .o_valid			(core_write_ready_w),
+    .o_alignment_score	(alignment_score),
+    .o_column			(column),
+    .o_row				(row)
 );
 
 task StartRead;
@@ -98,6 +102,19 @@ endtask
 
 // TODO
 always_comb begin
+    state_w = state_r;
+    bytes_counter_w = bytes_counter_r;
+    wrapper_read_ready_w = wrapper_read_ready_r;
+    wrapper_write_ready_w = wrapper_write_ready_r;
+
+    avm_address_w = avm_address_r;
+    avm_read_w = avm_read_r;
+    avm_write_w = avm_write_r;
+
+    ref_w = ref_r;
+    read_w = read_r;
+
+    result_w = result_r;
     case (state_r) 
         S_QUERY_RX: begin
             if (~avm_waitrequest && avm_readdata[RX_OK_BIT] == 1) begin
@@ -117,7 +134,7 @@ always_comb begin
                     ref_w = {ref_r[2*`REF_MAX_LENGTH-9:0], avm_readdata[7:0]};
                     wrapper_read_ready_w = 0;
                 end
-                else (bytes_counter_r <= (`READ_MAX_LENGTH >> 2)) begin
+                else if (bytes_counter_r < ((`READ_MAX_LENGTH + `REF_MAX_LENGTH) >> 2)) begin
                     // read sequence
                     state_w = S_QUERY_RX;
                     read_w = {read_r[2*`READ_MAX_LENGTH-9:0], avm_readdata[7:0]};
@@ -129,15 +146,18 @@ always_comb begin
                     read_w = {read_r[2*`READ_MAX_LENGTH-9:0], avm_readdata[7:0]};
                     bytes_counter_w = 0;
                     wrapper_read_ready_w = 1;
+                    avm_read_w = 0;
                 end
             end
         end
         S_CALC: begin
-            if (core_write_ready_r) begin
+            if (core_write_ready_w) begin
                 if (~wrapper_write_ready_r) begin
                     wrapper_write_ready_w = 1;
                 end else begin
-                    result_w[191:0] = {column_r[63:0], row_r[63:0], alignment_score_r[63:0]};
+                    result_w[`DP_SW_SCORE_BITWIDTH-1-:`DP_SW_SCORE_BITWIDTH] = alignment_score;
+                    result_w[63+$clog2(`READ_MAX_LENGTH)-:$clog2(`READ_MAX_LENGTH)] = row;
+                    result_w[127+$clog2(`REF_MAX_LENGTH)-:$clog2(`REF_MAX_LENGTH)] = column;
                     state_w = S_QUERY_TX;
                     wrapper_write_ready_w = 0;
                 end
@@ -148,13 +168,17 @@ always_comb begin
                 StartWrite(TX_BASE);
                 state_w = S_WRITE;
                 bytes_counter_w = bytes_counter_r + 1;
+            end else begin
+                StartRead(STATUS_BASE);
             end
         end
         S_WRITE: begin
             if (~avm_waitrequest) begin
                 StartRead(STATUS_BASE);
                 if (bytes_counter_r == 31) begin
+                    bytes_counter_w = 0;
                     state_w = S_QUERY_RX;
+                    avm_write_w = 0;
                 end else begin
                     state_w = S_QUERY_TX;
                     result_w = {result_r[239:0], result_r[247:240]};    
@@ -164,6 +188,7 @@ always_comb begin
         default: begin
 
         end
+    endcase
 end
 
 // TODO
@@ -171,9 +196,6 @@ always_ff @(posedge avm_clk or posedge avm_rst) begin
     if (avm_rst) begin
     	ref_r <= 0;
         read_r <= 0;
-        alignment_score_r <= 0;
-        column_r <= 0;
-        row_r <= 0;
         wrapper_read_ready_r <= 0;
         wrapper_write_ready_r <= 0;
         state_r <= S_QUERY_RX;
@@ -182,6 +204,8 @@ always_ff @(posedge avm_clk or posedge avm_rst) begin
         avm_address_r <= STATUS_BASE;
         avm_read_r <= 1;
         avm_write_r <= 0;
+        ref_length_r <= `REF_LENGTH;
+        read_length_r <= `READ_LENGTH;
     end
 	else begin
     	ref_r <= ref_w;
