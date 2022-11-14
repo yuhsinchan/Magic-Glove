b1dd467a3db043f69894699d38d60e585ab78a93
allenwu9999 <b08901057@ntu.edu.tw>
2022-11-15 02:11:45 +0800

    error: matrice update left behind a cycle

--- lab_bouns_SW/src/SW_Core_v2.sv
+++ lab_bouns_SW/src/SW_Core_v2.sv
@@ -83,6 +83,7 @@ module SW_core(
 
                     .i_B_base_valid             (sequence_B_valid[gv]                       ),
                     .i_B_base                   (sequence_B[2*`READ_MAX_LENGTH-1-(2*gv)-:2] ),
+                    .i_debug                    (0),
 
                     .i_align_top_score          ({(`DP_SW_SCORE_BITWIDTH){1'b0}}            ), // (0),
                     .i_insert_top_score         ({(`DP_SW_SCORE_BITWIDTH){1'b0}}            ), // (0),
@@ -113,6 +114,7 @@ module SW_core(
                     .i_A_base                   (PE_last_A_base[gv-1]                       ),
                     .i_B_base_valid             (sequence_B_valid[gv]                       ),
                     .i_B_base                   (sequence_B[2*`READ_MAX_LENGTH-1-(2*gv)-:2] ),
+                    .i_debug                    (gv == 1),
                     
                     .i_align_diagonal_score     (PE_align_score_dd [gv-1]                   ),
                     .i_align_top_score          (PE_align_score_d  [gv-1]                   ),
@@ -151,6 +153,7 @@ module SW_core(
 
     ///////////////////// main design ///////////////////
     always@(*) begin
+        // $display("length: %d, %d", seq_A_length, seq_B_length);
         sequence_A_n                                                            = sequence_A;
         sequence_B_n                                                            = sequence_B;
         seq_A_length_n                                                          = seq_A_length;
@@ -182,23 +185,85 @@ module SW_core(
         // *** TODO
         case(state)
             S_idle: begin
-                
+                // $display("idle state");
+                for (i=0;i<`READ_MAX_LENGTH;i=i+1) begin
+                    highest_score_n = MOST_NEGATIVE;
+                    column_n = 0;
+                    row_n = 0;
+                    row_highest_scores_n[i] = 0;
+                    row_highest_columns_n[i] = 0;
+                    PE_align_score_d_n[i] = 0;
+                    PE_insert_score_d_n[i] = 0;
+                    PE_delete_score_d_n[i] = 0;
+                    PE_align_score_dd_n[i] = 0;
+                    PE_insert_score_dd_n[i] = 0;
+                    PE_delete_score_dd_n[i] = 0;
+                    PE_score_buff_n[i] = 0;
+                    counter_n = 0;
+                end
+
+                o_ready = 1;
             end
 
             S_input: begin
-               
+                for (i=0;i<`READ_MAX_LENGTH;i=i+1) sequence_B_valid_n[i] = 1;
             end
 
             S_calculate: begin
-                           
+                // $display("calculate state");
+                $display("counter: %d", counter);
+                // $display("seqA+SeqB: %d", seq_A_length_n + seq_B_length_n);
+                counter_n = counter + 1;
+                sequence_A_shifter_n = {sequence_A_shifter[2*`REF_MAX_LENGTH-3:0], sequence_A_shifter[2*`REF_MAX_LENGTH-1-:2]};
+                // $display("sequence: %b", sequence_A_shifter);
+                for (i=0;i<seq_B_length;i=i+1) begin
+                    if (counter >= i+1 && counter-i <= seq_A_length) begin
+                        if (PE_score_buff_n[i] > row_highest_scores[i]) begin
+                            // $display("higher, update");
+                            row_highest_scores_n[i] = PE_score_buff_n[i];
+                            row_highest_columns_n[i] = counter - i - 1;
+                        end
+                    end
+                end
+                for (i=0;i<seq_B_length;i=i+1) begin
+                    if (PE_last_A_base_valid[i]) begin
+                        PE_align_score_d_n[i] = PE_align_score[i];
+                        PE_insert_score_d_n[i] = PE_insert_score[i];
+                        PE_delete_score_d_n[i] = PE_delete_score[i];
+                    end
+                end
+
+                for (i=0;i<seq_B_length;i=i+1) begin
+                    if (PE_last_A_base_valid[i]) begin
+                        PE_align_score_dd_n[i] = PE_align_score_d[i];
+                        PE_insert_score_dd_n[i] = PE_insert_score_d[i];
+                        PE_delete_score_dd_n[i] = PE_delete_score_d[i];
+                    end
+                end
+
+                if (counter == seq_A_length + seq_B_length - 1) begin
+                    for (i=0;i<`READ_MAX_LENGTH;i=i+1) sequence_B_valid_n[i] = 0;
+                    counter_n = 0;
+                end
             end
 
             S_select_highest: begin
-                
+                // $display("select highest state");
+                // $display("counter: %d", counter);
+                counter_n = counter + 1;
+                if (row_highest_scores[counter] > highest_score) begin
+                    highest_score_n = row_highest_scores[counter];
+                    column_n = row_highest_columns[counter];
+                    row_n = counter;
+                end
+                // $display("counter, score, col: %d, %d, %d", counter, row_highest_scores[counter], row_highest_columns[counter]);
             end
 
             S_done: begin
-                
+                // $display("done state");
+                o_row_n = row;
+                o_column_n = column;
+                o_valid_n = 1;
             end
         endcase
     end
@@ -235,14 +300,23 @@ module SW_core(
             o_row                       <= 0;
         end 
         else begin
+            if (i_valid) begin
+                sequence_A                                                  <= i_sequence_ref;
+                sequence_B                                                  <= i_sequence_read;
+                seq_A_length                                                <= i_seq_ref_length;
+                seq_B_length                                                <= i_seq_read_length;
+                sequence_A_shifter                                          <= i_sequence_ref;
+            end else begin
+                sequence_A                                                  <= sequence_A_n;
+                sequence_B                                                  <= sequence_B_n;
+                seq_A_length                                                <= seq_A_length_n;
+                seq_B_length                                                <= seq_B_length_n;
+                sequence_A_shifter                                          <= sequence_A_shifter_n;
+            end
             state                                                       <= state_n;
             counter                                                     <= counter_n;
-            sequence_A                                                  <= sequence_A_n;
-            sequence_B                                                  <= sequence_B_n;
-            seq_A_length                                                <= seq_A_length_n;
-            seq_B_length                                                <= seq_B_length_n;
+            
             for (i=0;i<`READ_MAX_LENGTH;i=i+1) sequence_B_valid[i]      <= sequence_B_valid_n[i];
-            sequence_A_shifter                                          <= sequence_A_shifter_n;
             
             highest_score                                               <= highest_score_n;
             column                                                      <= column_n;
@@ -278,6 +352,7 @@ module DP_PE_single(
     input                                       i_B_base_valid,
     input [1:0]                                 i_A_base,          // reference one.   Mapping: reference sequence
     input [1:0]                                 i_B_base,          // query one.       Mapping: short-read
+    input                                       i_debug,
     
     input signed [`DP_SW_SCORE_BITWIDTH-1:0]    i_align_diagonal_score,
     input signed [`DP_SW_SCORE_BITWIDTH-1:0]    i_align_top_score,
@@ -290,11 +365,11 @@ module DP_PE_single(
     input signed [`DP_SW_SCORE_BITWIDTH-1:0]    i_delete_diagonal_score,
     input signed [`DP_SW_SCORE_BITWIDTH-1:0]    i_delete_left_score,
 
-    output signed [`DP_SW_SCORE_BITWIDTH-1:0]   o_align_score,
-    output signed [`DP_SW_SCORE_BITWIDTH-1:0]   o_insert_score,
-    output signed [`DP_SW_SCORE_BITWIDTH-1:0]   o_delete_score,
+    output reg signed [`DP_SW_SCORE_BITWIDTH-1:0]   o_align_score,
+    output reg signed [`DP_SW_SCORE_BITWIDTH-1:0]   o_insert_score,
+    output reg signed [`DP_SW_SCORE_BITWIDTH-1:0]   o_delete_score,
 
-    output signed [`DP_SW_SCORE_BITWIDTH-1:0]   o_the_score,            // The highest score among o_align_score, o_insert_score and o_delete_score
+    output reg signed [`DP_SW_SCORE_BITWIDTH-1:0]   o_the_score,            // The highest score among o_align_score, o_insert_score and o_delete_score
     output reg                                  o_last_A_base_valid,
     output reg [1:0]                            o_last_A_base
 );
@@ -308,6 +383,55 @@ always@(posedge clk or posedge rst) begin
     end else begin
         o_last_A_base_valid <= i_A_base_valid;
         o_last_A_base       <= i_A_base;
+        if (i_A_base_valid && i_B_base_valid) begin
+            if (i_debug) begin
+                $display("A, B base: %d, %d", i_A_base, i_B_base);
+                $display("i_align_diag: %d", i_align_diagonal_score);
+                $display("i_align_top: %d", i_align_top_score);
+            end
+            if (i_A_base == i_B_base) begin
+                if (i_align_diagonal_score > i_insert_diagonal_score && i_align_diagonal_score > i_delete_diagonal_score) begin
+                    o_align_score <= i_align_diagonal_score + `CONST_MATCH_SCORE;
+                    o_the_score <= i_align_diagonal_score + `CONST_MATCH_SCORE;
+                end else if (i_insert_diagonal_score > i_delete_diagonal_score) begin
+                    o_align_score <= i_insert_diagonal_score + `CONST_MATCH_SCORE;
+                    o_the_score <= i_insert_diagonal_score + `CONST_MATCH_SCORE;
+                end else begin
+                    o_align_score <= i_delete_diagonal_score + `CONST_MATCH_SCORE;
+                    o_the_score <= i_delete_diagonal_score + `CONST_MATCH_SCORE;
+                end
+            end else begin
+                if (i_align_diagonal_score > i_insert_diagonal_score && i_align_diagonal_score > i_delete_diagonal_score && i_align_diagonal_score + `CONST_MISMATCH_SCORE > 0) begin
+                    o_align_score <= i_align_diagonal_score + `CONST_MISMATCH_SCORE;
+                    o_the_score <= i_align_diagonal_score + `CONST_MISMATCH_SCORE;
+                end else if (i_insert_diagonal_score > i_delete_diagonal_score && i_insert_diagonal_score + `CONST_MISMATCH_SCORE > 0) begin
+                    o_align_score <= i_insert_diagonal_score + `CONST_MISMATCH_SCORE;
+                    o_the_score <= i_insert_diagonal_score + `CONST_MISMATCH_SCORE;
+                end else if (i_delete_diagonal_score + `CONST_MISMATCH_SCORE > 0) begin
+                    o_align_score <= i_delete_diagonal_score + `CONST_MISMATCH_SCORE;
+                    o_the_score <= i_delete_diagonal_score + `CONST_MISMATCH_SCORE;
+                end else begin
+                    o_align_score <= 0;
+                    o_the_score <= 0;
+                end
+            end
+
+            if (i_align_top_score + `CONST_GAP_OPEN > i_insert_top_score + `CONST_GAP_EXTEND && i_align_top_score + `CONST_GAP_OPEN > 0) begin
+                o_insert_score <= i_align_top_score + `CONST_GAP_OPEN;
+            end else if (i_insert_left_score + `CONST_GAP_EXTEND > 0) begin
+                o_insert_score <= i_insert_top_score + `CONST_GAP_EXTEND;
+            end else begin
+                o_insert_score <= 0;
+            end
+
+            if (i_align_left_score + `CONST_GAP_OPEN > i_delete_left_score + `CONST_GAP_EXTEND && i_align_left_score + `CONST_GAP_OPEN > 0) begin
+                o_delete_score <= i_align_left_score + `CONST_GAP_OPEN;
+            end else if (i_delete_left_score + `CONST_GAP_EXTEND > 0) begin
+                o_delete_score <= i_delete_left_score + `CONST_GAP_EXTEND;
+            end else begin
+                o_delete_score <= 0;
+            end
+        end
     end
 end
 
